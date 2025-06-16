b5a340b707259f3f3f579abaf3932dab856419d9
FrancescoCellitti <francescocellitti@virgilio.it>
2025-06-16 15:48:15 +0200

    list added in html for the list of email

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,16 @@
+const liEl = document.querySelector('li')
+const ulEl = document.querySelector('ul')
 function generateEmail() {
     for (let i = 0; i < 10; i++) {
         fetch("https://flynn.boolean.careers/exercises/api/random/mail")
 
             .then(response => response.json())
             .then(email => {
+                const li = document.createElement('li');
+                li.innerText = email.response
                 console.log(email);
+                ulEl.appendChild(li)
+
             })
             .catch(error => {
                 console.error(error);
@@ -13,3 +19,5 @@ function generateEmail() {
 }
 
 generateEmail()
+
+console.log()
